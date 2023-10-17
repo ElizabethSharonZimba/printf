d813682ba43fb8ab650dda158677f7cd05b5b800
ElizabethSharonZimba <sharonzimba95@gmail.com>
2023-10-16 23:54:34 -0700

    .

--- conversions.c
+++ conversions.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
+unsigned int convert_ubase(buffer_t *output,
+		unsigned long int num, char *base,
+		unsigned char flags, int wid, int prec);
+
 /**
  * convert_sbase - Converts long to base then stores
  * @output: buffer_t that has char array
